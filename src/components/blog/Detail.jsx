0770f18fe4dd07c8461b029ec5e1658277fea030
imkeri <keyuriribadiya2710@gmail.com>
2024-09-27 21:55:57 +0530

    blog ui

---
+++ src/components/blog/Detail.jsx
@@ -0,0 +1,130 @@
+
+
+function Detail() {
+    return (
+      <div class="blog-single gray-bg">
+          <div class="container">
+              <div class="row align-items-start">
+                  <div class="col-lg-8 m-15px-tb">
+                      <article class="article">
+                          <div class="article-img">
+                              <img src="./images.jpg" title="" alt=""/>
+                          </div>
+                          <div class="article-title">
+                              <button className='tags'>Lifestyle</button>
+                              <h2>They Now Bade Farewell To The Kind But Unseen People</h2>
+                              <div class="media">
+                                  <div class="media-body">
+                                      <label>Rachel Roth</label>
+                                      <span>26 FEB 2020</span>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="article-content">
+                              <p>Aenean eleifend ante maecenas pulvinar montes lorem et pede dis dolor pretium donec dictum. Vici consequat justo enim. Venenatis eget adipiscing luctus lorem. Adipiscing veni amet luctus enim sem libero tellus viverra venenatis aliquam. Commodo natoque quam pulvinar elit.</p>
+                              <p>Eget aenean tellus venenatis. Donec odio tempus. Felis arcu pretium metus nullam quam aenean sociis quis sem neque vici libero. Venenatis nullam fringilla pretium magnis aliquam nunc vulputate integer augue ultricies cras. Eget viverra feugiat cras ut. Sit natoque montes tempus ligula eget vitae pede rhoncus maecenas consectetuer commodo condimentum aenean.</p>
+                              <h4>What are my payment options?</h4>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                              <blockquote>
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                                  <p class="blockquote-footer">Someone famous in <cite title="Source Title">Dick Grayson</cite></p>
+                              </blockquote>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                          </div>
+                      
+                      </article>
+                  </div>
+                  <div class="col-lg-4 m-15px-tb blog-aside">
+                  <div class="widget widget-tags">
+                          <div class="widget-title">
+                              <h3>Latest Tags</h3>
+                          </div>
+                          <div class="widget-body">
+                              <div class="nav tag-cloud">
+                                  <button className='tags'>Design</button>
+                                  <button className='tags'>Development</button>
+                                  <button className='tags'>Travel</button>
+                                  <button className='tags'>Web Design</button>
+                                  <button className='tags'>Marketing</button>
+                                  <button className='tags'>Research</button>
+                                  <button className='tags'>Managment</button>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="widget widget-latest-post">
+                          <div class="widget-title">
+                              <h3>Latest Post</h3>
+                          </div>
+                          <div class="widget-body">
+                              <div class="d-flex latest-post-aside media">
+                                  <div class="lpa-left media-body">
+                                      <div class="lpa-title">
+                                          <h5><a href="#">Prevent 75% of visitors from google analytics</a></h5>
+                                      </div>
+                                      <div class="lpa-meta">
+                                          <a class="name" href="#">
+                                          <i class="fa fa-pencil-square-o"></i>
+                                              Rachel Roth
+                                          </a>
+                                          <a class="date" href="#">
+                                              26 FEB 2020
+                                          </a>
+                                      </div>
+                                  </div>
+                                  <div class="lpa-right">
+                                      <a href="#">
+                                          <img src="./images.jpg" title="" alt=""/>
+                                      </a>
+                                  </div>
+                              </div>
+                              <div class=" d-flex latest-post-aside media">
+                                  <div class="lpa-left media-body">
+                                      <div class="lpa-title">
+                                          <h5><a href="#">Prevent 75% of visitors from google analytics</a></h5>
+                                      </div>
+                                      <div class="lpa-meta">
+                                          <a class="name" href="#">
+                                              Rachel Roth
+                                          </a>
+                                          <a class="date" href="#">
+                                              26 FEB 2020
+                                          </a>
+                                      </div>
+                                  </div>
+                                  <div class="lpa-right">
+                                      <a href="#">
+                                          <img src="./images.jpg" title="" alt=""/>
+                                      </a>
+                                  </div>
+                              </div>
+                              <div class="d-flex latest-post-aside media">
+                                  <div class="lpa-left media-body">
+                                      <div class="lpa-title">
+                                          <h5><a href="#">Prevent 75% of visitors from google analytics</a></h5>
+                                      </div>
+                                      <div class="lpa-meta">
+                                          <a class="name" href="#">
+                                              Rachel Roth
+                                          </a>
+                                          <a class="date" href="#">
+                                              26 FEB 2020
+                                          </a>
+                                      </div>
+                                  </div>
+                                  <div class="lpa-right">
+                                      <a href="#">
+                                          <img src="./images.jpg" title="" alt=""/>
+                                      </a>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    );
+  }
+  
+  export default Detail;
+  
---
+++ src/components/blog/List.jsx
@@ -0,0 +1,223 @@
+import React from 'react'
+
+const DetailList = () => {
+    return (
+        <div id="main-content" class="blog-page">
+            <div class="container">
+                <div class="row clearfix">
+                    <div class="col-lg-8 col-md-12 left-box">
+                        <div class="card single_post">
+                            <div class="body">
+                                <div class="img-post">
+                                    <img class="d-block img-fluid" src="./images.jpg" alt="First slide" />
+                                </div>
+                                <h3><a href="blog-details.html">All photographs are accurate</a></h3>
+                                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal</p>
+                            </div>
+                            <div class="footer">
+                                <div class="actions">
+                                    <a href="javascript:void(0);" class="btn btn-outline-secondary">Continue Reading</a>
+                                </div>
+                                <ul class="stats">
+                                    <li><a href="javascript:void(0);">General</a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-heart">28</a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-comment">128</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="card single_post">
+                            <div class="body">
+                                <div class="img-post">
+                                    <img class="d-block img-fluid" src="./images.jpg" alt="" />
+                                </div>
+                                <button className='tags mb-2'>Lifestyle</button>
+                                <h3><a href="blog-details.html">All photographs are accurate</a></h3>
+                                <div class="author">
+                                    <i class="fa fa-pencil-square-o"></i>
+                                    <div class="media-body">
+                                        <label>Rachel Roth</label>
+                                        <span>26 FEB 2020</span>
+                                    </div>
+                               
+                                </div>
+                                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal</p>
+                            </div>
+                            <div class="footer">
+                                <div class="actions">
+                                    <a href="javascript:void(0);" class="btn btn-outline-secondary">Continue Reading</a>
+                                </div>
+                                <ul class="stats">
+                                    <li><a href="javascript:void(0);">General</a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-heart">28</a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-comment">128</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="card single_post">
+                            <div class="body">
+                                <div class="img-post">
+                                    <img class="d-block img-fluid" src="./images.jpg" alt="" />
+                                </div>
+                                <button className='tags mb-2'>Lifestyle</button>
+                                <h3><a href="blog-details.html">All photographs are accurate</a></h3>
+                                <div class="author">
+                                    <i class="fa fa-pencil-square-o"></i>
+                                    <div class="media-body">
+                                        <label>Rachel Roth</label>
+                                        <span>26 FEB 2020</span>
+                                    </div>
+                               
+                                </div>
+                                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal</p>
+                            </div>
+                            <div class="footer">
+                                <div class="actions">
+                                    <a href="javascript:void(0);" class="btn btn-outline-secondary">Continue Reading</a>
+                                </div>
+                                <ul class="stats">
+                                    <li><a href="javascript:void(0);">General</a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-heart">28</a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-comment">128</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="card single_post">
+                            <div class="body">
+                                <div class="img-post">
+                                    <img class="d-block img-fluid" src="./images.jpg" alt="" />
+                                </div>
+                                <button className='tags mb-2'>Lifestyle</button>
+                                <h3><a href="blog-details.html">All photographs are accurate</a></h3>
+                                <div class="author">
+                                    <i class="fa fa-pencil-square-o"></i>
+                                    <div class="media-body">
+                                        <label>Rachel Roth</label>
+                                        <span>26 FEB 2020</span>
+                                    </div>
+                               
+                                </div>
+                                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal</p>
+                            </div>
+                            <div class="footer">
+                                <div class="actions">
+                                    <a href="javascript:void(0);" class="btn btn-outline-secondary">Continue Reading</a>
+                                </div>
+                                <ul class="stats">
+                                    <li><a href="javascript:void(0);">General</a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-heart">28</a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-comment">128</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12 right-box">
+                        <div class="card">
+                            <div class="body search">
+                                <div class="input-group m-b-0">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fa fa-search"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" placeholder="Search..." />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="widget-title">
+                                <h3>Categories Clouds</h3>
+                            </div>
+                            <div class="body widget">
+                                <ul class="list-unstyled categories-clouds m-b-0">
+                                    <li><button className='tags'>eCommerce</button></li>
+                                    <li><button className='tags'>Microsoft Technologies</button></li>
+                                    <li><button className='tags'>Creative UX</button></li>
+                                    <li><button className='tags'>Wordpress</button></li>
+                                    <li><button className='tags'>Angular JS</button></li>
+                                    <li><button className='tags'>Enterprise Mobility</button></li>
+                                    <li><button className='tags'>Website Design</button></li>
+                                    <li><button className='tags'>HTML5</button></li>
+                                    <li><button className='tags'>Infographics</button></li>
+                                    <li><button className='tags'>Wordpress Development</button></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class=" blog-aside">
+                            <div class="widget widget-latest-post">
+                                <div class="widget-title">
+                                    <h3>Latest Post</h3>
+                                </div>
+                                <div class="widget-body">
+                                    <div class="d-flex latest-post-aside media">
+                                        <div class="lpa-left media-body">
+                                            <div class="lpa-title">
+                                                <h5><a href="#">Prevent 75% of visitors from google analytics</a></h5>
+                                            </div>
+                                            <div class="lpa-meta">
+                                                <a class="name" href="#">
+                                                    <i class="fa fa-pencil-square-o"></i>
+                                                    Rachel Roth
+                                                </a>
+                                                <a class="date" href="#">
+                                                    26 FEB 2020
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="lpa-right">
+                                            <a href="#">
+                                                <img src="./images.jpg" title="" alt="" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class=" d-flex latest-post-aside media">
+                                        <div class="lpa-left media-body">
+                                            <div class="lpa-title">
+                                                <h5><a href="#">Prevent 75% of visitors from google analytics</a></h5>
+                                            </div>
+                                            <div class="lpa-meta">
+                                                <a class="name" href="#">
+                                                    Rachel Roth
+                                                </a>
+                                                <a class="date" href="#">
+                                                    26 FEB 2020
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="lpa-right">
+                                            <a href="#">
+                                                <img src="./images.jpg" title="" alt="" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex latest-post-aside media">
+                                        <div class="lpa-left media-body">
+                                            <div class="lpa-title">
+                                                <h5><a href="#">Prevent 75% of visitors from google analytics</a></h5>
+                                            </div>
+                                            <div class="lpa-meta">
+                                                <a class="name" href="#">
+                                                    Rachel Roth
+                                                </a>
+                                                <a class="date" href="#">
+                                                    26 FEB 2020
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="lpa-right">
+                                            <a href="#">
+                                                <img src="./images.jpg" title="" alt="" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DetailList
